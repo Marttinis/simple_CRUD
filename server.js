10717ffc05bdf24f215b8136afacb37c00c04164
Gabriel Martins <gabrielmartins23445@gmail.com>
2025-02-27 10:03:25 -0300

    finalizei o get, post e delete do crud

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const app = express()
 
-const {PrismaClient} = require("@prisma/client")
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+
 
 // const cor = require("cors");
 // app.use(cors());
@@ -10,28 +11,39 @@ const prisma = new PrismaClient();
 const PORT = 4000
 app.use(express.json());
 
-app.get("/filme", async (req, res) =>{
+app.get("/filme", async (req, res) => {
     const filmes = await prisma.filme.findMany();
     res.json(filmes)
-} )
-
-app.get("/filme/:id", async (req,res) => {
-    const id = req.params.id
-    const filme = await prisma.filme.findUnique({ where: {id} })
-    if (!filme) return res.status(400).json({error: "filme não encontrado"})
+})
+ 
+app.get("/filme/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    const filme = await prisma.filme.findUnique({ where: { id } }) 
+    if (!filme) return res.status(400).json({ error: "filme não encontrado" })
 
     res.json(filme);
 })
 
-app.post("/filme" , async (req,res) =>{
-    const body = req.body;
-    const newFilme = await prisma.filme.create(body)
-
+app.post("/filme", async (req, res) => { 
+    const body = req.body; 
+    const newFilme = await prisma.filme.create({data: body}) 
     res.status(201).json(newFilme);
+ 
+}) 
+
+
+app.delete("/filme/:id", async (req, res)=>{
+    const id = parseInt(req.params.id)
+    const filmeExiste = await prisma.filme.findUnique({where: {id}})
+
+    if (!filmeExiste) return res.status(400).json({error: "filme não encontrado"})
+    
+    await prisma.filme.delete({where: {id} });
+    res.status(200).json({message: "Filme excluido com sucesso"})
 })
 
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`Servidor rodando na porta http://localhost:${PORT}`);
-    
+
 })
